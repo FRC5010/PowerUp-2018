1bbd4ac827622df60066c4a759c0f45fbdfa87a4
5010TigerDynasty <roboticsteam.fhs@gmail.com>
2018-01-29 16:15:38 -0500

    OI for shifting

--- PowerUp2018/src/org/usfirst/frc/team5010/robot/OI.java
+++ PowerUp2018/src/org/usfirst/frc/team5010/robot/OI.java
@@ -7,6 +7,8 @@
 
 package org.usfirst.frc.team5010.robot;
 
+import org.usfirst.frc.team5010.robot.commands.ShiftDown;
+import org.usfirst.frc.team5010.robot.commands.ShiftUp;
 import org.usfirst.frc.team5010.robot.commands.SwitchDriveMode;
 import org.usfirst.frc.team5010.robot.commands.TurnToAngle;
 
@@ -49,16 +51,20 @@ public class OI {
 	// until it is finished as determined by it's isFinished method.
 	// button.whenReleased(new ExampleCommand());
 	public Joystick joyDriver = new Joystick(0);
-	public Joystick joyDriver2 = new Joystick(1);
-	public Joystick joyCoDriver = new Joystick(2);
+	//public Joystick joyDriver2 = new Joystick(1);
+	public Joystick joyCoDriver = new Joystick(1);
 	
 	
 	private Button buttonY = new JoystickButton(joyDriver, 4);
 	private Button buttonBack = new JoystickButton(joyDriver,7);
+	private Button buttonRB = new JoystickButton(joyDriver, 6);
+	private Button buttonLB = new JoystickButton(joyDriver, 5);
 	
 	public OI () {
 		buttonY.whenPressed(new SwitchDriveMode());
 		buttonBack.whenReleased(new TurnToAngle(45));
+		buttonRB.whenPressed(new ShiftUp());
+		buttonLB.whenPressed(new ShiftDown());
 		joyDriver.getName();
 		SmartDashboard.putString("joystick Name", joyDriver.getName());
 	}
