2e3de7a2b35be753e6cc2f92f71eb9347b7fd076
Collin-T <35089250+Collin-T@users.noreply.github.com>
2018-03-29 10:44:05 -0400

    Update DoublePointer.java

--- PowerUp2018/src/org/usfirst/frc/team5010/robot/jetson_autonomous/DoublePointer.java
+++ PowerUp2018/src/org/usfirst/frc/team5010/robot/jetson_autonomous/DoublePointer.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team5010.robot.jetson_autonomous;
 
+//A workaround to pass doubles by reference
 public class DoublePointer {
   public double val;
   public DoublePointer() {
