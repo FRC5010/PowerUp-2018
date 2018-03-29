63a31737ad2665eeaaca08090d5ddca5d45001e2
Collin-T <35089250+Collin-T@users.noreply.github.com>
2018-03-29 10:24:01 -0400

    Update CameraConstants.java

--- PowerUp2018/src/org/usfirst/frc/team5010/robot/jetson_autonomous/CameraConstants.java
+++ PowerUp2018/src/org/usfirst/frc/team5010/robot/jetson_autonomous/CameraConstants.java
@@ -1,3 +1,9 @@
 package org.usfirst.frc.team5010.robot.jetson_autonomous;
 
-public class 
+public class CameraConstants {
+    public static final double HEIGHT = 0;
+    public static final double THETA_Y_XZ = 0;
+    public static final double FOCAL_LENGTH = 0;
+    public static final double CAMERA_ANGLE_X_YZ_INTERVAL_SIZE = 0;
+    public static final double CAMERA_ANGLE_X_YZ_OFFSET = 0;
+}
